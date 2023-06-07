436649fcac234648951a1553b6b90149c494239d
James Yu Cao <caoyu19920414@gmail.com>
2023-06-07 19:46:32 -0400

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // parse and verify the token
@@ -8,4 +9,18 @@ const { restoreUser } = require('../../utils/auth.js');
 // with attributes: email, createdAt, updatedAt
 router.use(restoreUser);
 
+// Login: POST / api / session
+// Logout: DELETE / api / session
+// Get session user: GET / api / session
+router.use('/session', sessionRouter);
+
+// Signup: POST /api/users
+router.use('/users', usersRouter);
+
+// Test: POST /api/test
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+// export default
 module.exports = router;
