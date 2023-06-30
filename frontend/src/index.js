8299570adaa30bddfc40f58083e9f24f9a638705
James Yu Cao <caoyu19920414@gmail.com>
2023-06-30 13:14:15 -0400

    finished the Authenticate Me Phase 0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// import Provider from react-redux
 import { Provider } from 'react-redux';
+
+// BrowserRouter
 import { BrowserRouter } from 'react-router-dom';
+
+// import the config store from redux folder
 import configureStore from './store';
 
-// frontend/src/index.js
-// ...
+// custom fetch wrapped with XSRF-TOKEN
+// restore XSRF-TOKEN function for developement mode
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+// create the redux store
 const store = configureStore();
 
+// when in developement mode,
+// use restoreCSRF to get the XSRF-TOKEN
+// expose the redux store to the browser window object
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
-// frontend/src/index.js
-// ...
+// pass the created redux store to the provider
+// and wrap the app with it
 function Root() {
   return (
     <Provider store={store}>
@@ -27,8 +40,7 @@ function Root() {
   );
 }
 
-// frontend/src/index.js
-// ...
+
 ReactDOM.render(
   <React.StrictMode>
     <Root />
